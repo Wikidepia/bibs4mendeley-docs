0dba056cc05f25573f10827d3787db347d315111
Akmal <magchuz@pm.me>
2024-04-30 16:57:59 +0700

    Refactor auth to use code authorization

--- src/auth.ts
+++ src/auth.ts
@@ -1,57 +1,90 @@
-/**
- * Configures the service.
- */
-function getService_() {
-  const scriptProp = PropertiesService.getScriptProperties();
-  const clientID = scriptProp.getProperty("OAUTH2_CLIENT_ID");
-  const clientSecret = scriptProp.getProperty("OAUTH2_CLIENT_SECRET");
+function refreshToken(): string {
+  var userProperties = PropertiesService.getUserProperties();
+  var refresh_token = userProperties.getProperty("REFRESH_TOKEN");
 
-  if (!clientID || !clientSecret) {
+  var scriptProperties = PropertiesService.getScriptProperties();
+  var client_id = scriptProperties.getProperty("OAUTH2_CLIENT_ID");
+  var client_secret = scriptProperties.getProperty("OAUTH2_CLIENT_SECRET");
+
+  // Generate initial access token
+  var response = UrlFetchApp.fetch("https://api.mendeley.com/oauth/token", {
+    method: "post",
+    contentType: "application/x-www-form-urlencoded",
+    headers: {
+      Authorization:
+        "Basic " + Utilities.base64Encode(`${client_id}:${client_secret}`),
+    },
+    payload: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+      redirect_uri: "https://bibs4mendeley.pages.dev",
+    },
+  });
+
+  var json = JSON.parse(response.getContentText());
+  var current_time = Math.floor(Date.now() / 1000);
+  userProperties.setProperty("ACCESS_TOKEN", json.access_token);
+  userProperties.setProperty("REFRESH_TOKEN", json.refresh_token);
+  userProperties.setProperty("EXPIRE_TIME", current_time + json.expires_in);
+  return json.access_token;
+}
+
+function initAccessToken(code: string) {
+  var userProperties = PropertiesService.getUserProperties();
+  userProperties.setProperty("OAUTH2_CODE", code);
+
+  var scriptProperties = PropertiesService.getScriptProperties();
+  var client_id = scriptProperties.getProperty("OAUTH2_CLIENT_ID");
+  var client_secret = scriptProperties.getProperty("OAUTH2_CLIENT_SECRET");
+
+  // Generate initial access token
+  var response = UrlFetchApp.fetch("https://api.mendeley.com/oauth/token", {
+    method: "post",
+    contentType: "application/x-www-form-urlencoded",
+    headers: {
+      Authorization:
+        "Basic " + Utilities.base64Encode(`${client_id}:${client_secret}`),
+    },
+    payload: {
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: "https://bibs4mendeley.pages.dev",
+    },
+  });
+
+  var json = JSON.parse(response.getContentText());
+  var current_time = Math.floor(Date.now() / 1000);
+  var expires_time = current_time + json.expires_in - 30;
+  userProperties.setProperty("ACCESS_TOKEN", json.access_token);
+  userProperties.setProperty("REFRESH_TOKEN", json.refresh_token);
+  userProperties.setProperty("EXPIRE_TIME", expires_time.toString());
+
+  var ui = DocumentApp.getUi();
+  ui.alert("Access token generated successfully!");
+}
+
+function getAccessToken(): string {
+  var userProperties = PropertiesService.getUserProperties();
+  var current_time = Math.floor(Date.now() / 1000);
+  var expire_time = userProperties.getProperty("EXPIRE_TIME");
+  var access_token = userProperties.getProperty("ACCESS_TOKEN");
+
+  if (!expire_time || !access_token) {
+    mendeleyLogin();
     throw new Error(
-      "Script properties must be set to run the script. Please set the 'OAUTH2_CLIENT_ID' and 'OAUTH2_CLIENT_SECRET' properties."
+      "Access token not found! Please connect your Mendeley account."
     );
   }
 
-  return (
-    OAuth2.createService("Mendeley")
-      // Set the endpoint URLs.
-      .setAuthorizationBaseUrl("https://api.mendeley.com/oauth/authorize")
-      .setTokenUrl("https://api.mendeley.com/oauth/token")
-
-      // Set the client ID and secret.
-      .setClientId(clientID)
-      .setClientSecret(clientSecret)
-
-      // Set the name of the callback function that should be invoked to
-      // complete the OAuth flow.
-      .setCallbackFunction("authCallback")
-
-      // Set the property store where authorized tokens should be persisted.
-      .setPropertyStore(PropertiesService.getUserProperties())
-
-      // Set the scope. The "signature" scope is used for all endpoints in the
-      // eSignature REST API.
-      .setScope("all")
-  );
-}
-
-/**
- * Handles the OAuth callback.
- */
-function authCallback(request: object) {
-  var service = getService_();
-  var authorized = service.handleCallback(request);
-  if (authorized) {
-    return HtmlService.createHtmlOutput("Success! You can close this tab.");
-  } else {
-    return HtmlService.createHtmlOutput("Failed to authorize, please try again.");
+  if (current_time > parseInt(expire_time)) {
+    access_token = refreshToken();
   }
+  return access_token;
 }
 
-/**
- * Logs the redict URI to register in the Dropbox application settings.
- */
-function logRedirectUri() {
-  var service = getService_();
-  Logger.log(service.getRedirectUri());
+function resetAccessToken() {
+  var userProperties = PropertiesService.getUserProperties();
+  userProperties.deleteProperty("ACCESS_TOKEN");
+  userProperties.deleteProperty("REFRESH_TOKEN");
+  userProperties.deleteProperty("EXPIRE_TIME");
 }
